87505b936d5b582a1bc4ad8364523a55417a00f9
Karthick Eswaran <karthickeswaran8@gmail.com>
2025-09-10 06:39:27 +0000

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import './sections/hero'
+import './sections/about'
+import './sections/work_process'
+import './sections/contact'
+
+function App() {
+
+  return (
+    <>
+    </>
+
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  server: {
+    watch: {
+      usePolling: true // Set to true if file changes aren’t detected (e.g., in WSL or network drives)
+    }
+  }
+
+})
